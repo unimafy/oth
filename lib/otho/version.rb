a8945f6a88f31ceea4a21b93ca51183d7f02d02a
Nima Yonten <92887110+unimafy@users.noreply.github.com>
2022-08-17 02:48:57 +0600

    initial commit

---
+++ lib/otho/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Otho
+  VERSION = "0.1.0"
+end
---
+++ spec/otho_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Otho do
+  it "has a version number" do
+    expect(Otho::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
